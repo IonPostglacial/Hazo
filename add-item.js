1741f6601241d220a71c5fee757f2fd3f2aedd0e
Nicolas Galipot <nicolas.galipot@gmail.com>
2020-05-07 12:33:18 +0200

    Add file forgoetten in previous commit. Ooops.

---
+++ add-item.js
@@ -0,0 +1,20 @@
+Vue.component("add-item", {
+    props: ["onAdd"],
+    methods: {
+        addItem(e) {
+            const input = e.target.tagName === "INPUT" ? e.target : e.target.previousElementSibling;
+            const val = input.value;
+            input.value = "";
+            console.log(val);
+            this.$emit("add-item", val);
+        }
+    },
+    template: `
+        <div class="horizontal-flexbox">
+            <input class="flex-grow-1" type="text" v-on:keydown.enter="addItem" v-on:input="$emit('input', $event.target.value)" />
+            <button class="background-color-1" v-on:click="addItem">
+                Add
+            </button>
+        </div>
+    `
+});
