36c3e139683cf3fa5be8e844c3746d044896a75d
Nicolas Galipot <nicolas.galipot@enablon.com>
2020-07-28 10:07:45 +0200

    Fix download of text data.

--- src/download.js
+++ src/download.js
@@ -1,7 +1,7 @@
 export default function download(content, extension, binary=false) {
     const filename = window.prompt("Choose a file name", "export") + `.${extension}`;
     const element = document.createElement("a");
-    const url = binary ? URL.createObjectURL(content) : ("data:text/plain;charset=utf-8" + encodeURIComponent(content));
+    const url = binary ? URL.createObjectURL(content) : ("data:text/plain;charset=utf-8," + encodeURIComponent(content));
     element.setAttribute("href", url);
     element.setAttribute("download", filename);
 
