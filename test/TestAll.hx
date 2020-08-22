c27787b829c8cc75ebb670c5ca65f6f169cd63b8
Nicolas Galipot <nicolas.galipot@gmail.com>
2020-08-22 16:35:39 +0200

    Fix unit tests.

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -6,7 +6,6 @@ class TestAll {
 		var runner = new Runner();
 		runner.addCase(new TestLoader());
 		runner.addCase(new TestSaver());
-		runner.addCase(new TestDetailData());
 		Report.create(runner);
 		runner.run();
 	}
