ba971e8952ec0009b2d21ba0860f2ecd60dba847
Nicolas Galipot <nicolas.galipot@gmail.com>
2020-11-02 21:54:20 +0100

    Add forgotten file.

---
+++ src/debounce.ts
@@ -0,0 +1,9 @@
+export default function debounce<T extends (...args: any[]) => any>(wait: number, func: T) {
+	let timeout: number|undefined = undefined;
+	return function(this: ThisParameterType<T>, ...args: Parameters<T>) {
+        if (typeof timeout !== "undefined") {
+            clearTimeout(timeout);
+        }
+        timeout = window.setTimeout(func.bind(this, args), wait);
+	};
+}
