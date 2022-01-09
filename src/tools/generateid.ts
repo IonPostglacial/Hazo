047ef64f455eeac1595c1d520c06465f2cbd196c
Nicolas Galipot <nicolas.galipot@gmail.com>
2022-01-09 13:57:50 +0100

    Fix generate id in exceptional cases.

--- src/tools/generateid.ts
+++ src/tools/generateid.ts
@@ -1,6 +1,6 @@
 export function generateId(prefix: string, items: { has(key: string): boolean, size: number }, item: { id: string }): string {
     let id = item.id;
-    if (id === "") {
+    if (!id) {
         let i = items.size;
         do {
             id = prefix + i;
