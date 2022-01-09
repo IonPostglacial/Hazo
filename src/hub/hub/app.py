884e5f98f92836d721f9b93cbf7746a1965c7f86
Nicolas Galipot <nicolas.galipot@gmail.com>
2022-01-09 17:17:27 +0100

    Remove fossils from the old Hub.

--- src/hub/hub/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from .database import LocalSession
-
-UPLOAD_FOLDER = '/tmp/hazo-hub'
-
-app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
