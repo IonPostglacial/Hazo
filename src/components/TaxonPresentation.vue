627f2433e4ab6cc119dc73425122a3040e3d2a38
Nicolas Galipot <nicolas.galipot@gmail.com>
2022-02-13 15:23:35 +0100

    Add the ability to select language for Taxon printing.

--- src/components/TaxonPresentation.vue
+++ src/components/TaxonPresentation.vue
@@ -2,6 +2,10 @@
     <div class="scroll flex-grow-1">
         <div class="horizontal-flexbox no-print">
             <router-link class="button" :to="'/taxons/' + this.selectedTaxonId">Back to Taxon</router-link>
+            <select v-model="lang">
+                <option value="S">Scientific</option>
+                <option value="CN">Chinese</option>
+            </select>
              <button type="button" class="background-color-1" @click="print">Print</button>
         </div>
         <article style="max-width: 100ch" class="centered white-background medium-padding">
@@ -27,9 +31,9 @@
                             <flowering v-if="isFlowering(description)" :value="monthsFromStates(description.states)">
                             </flowering>
                             <div v-if="!isFlowering(description)" class="horizontal-flexbox">
-                                <div>{{ description.character.name.S }}<span class="spaced">:</span></div>
+                                <div>{{ charName(description.character) }}<span class="spaced">:</span></div>
                                 <div v-for="(state, index) in description.states" :key="state.id">
-                                    <span v-if="index > 0" class="spaced">/</span> {{ state.name.S }}
+                                    <span v-if="index > 0" class="spaced">/</span> {{ stateName(state) }}
                                 </div>
                             </div>
                         </section>
@@ -45,7 +49,7 @@
 import { Dataset, Description, Taxon } from '@/datatypes'; // eslint-disable-line no-unused-vars
 import { forEachLeaves, iterHierarchy } from '@/datatypes/hierarchy';
 import Months from '@/datatypes/Months';
-import { State } from '@/datatypes/types';
+import { Character, State } from '@/datatypes/types';
 import Vue, { PropType } from "vue"; // eslint-disable-line no-unused-vars
 import Flowering from "./Flowering.vue";
 
@@ -56,6 +60,7 @@ export default Vue.extend({
         return {
             isParentSelected: {} as Record<string, boolean>,
             store: Hazo.store,
+            lang: "S",
             selectedTaxonId: this.$route.params.id ?? "",
         }
     },
@@ -72,18 +77,26 @@ export default Vue.extend({
             return this.dataset.taxon(this.selectedTaxonId);
         },
         itemsToDisplay(): Iterable<Taxon> {
-            if (typeof this.selectedTaxon !== "undefined") {
+            if (this.selectedTaxon) {
+                return iterHierarchy(this.selectedTaxon);
+            } else {
                 const items: Taxon[] = [];
                 forEachLeaves(this.dataset.taxonsHierarchy, t => {
                     items.push(t);
                 });
                 return items;
-            } else {
-                return iterHierarchy(this.dataset.taxonsHierarchy);
             }
         },
     },
     methods: {
+        charName(ch: Character): string {
+            const n:any = ch.name ?? {};
+            return n[this.lang];
+        },
+        stateName(s: State): string {
+            const n:any = s.name ?? {};
+            return n[this.lang];
+        },
         pictureUrl(item: { url: string, hubUrl: string|undefined }): string {
             if (item.hubUrl) {
                 return item.hubUrl; 
