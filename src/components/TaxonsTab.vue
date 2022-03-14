2a6f4027de01d6171fdb339c84b6c9126281ca49
Nicolas Galipot <nicolas.galipot@gmail.com>
2022-03-14 17:05:21 +0100

    Add images to the folder export.

--- src/components/TaxonsTab.vue
+++ src/components/TaxonsTab.vue
@@ -372,7 +372,7 @@ export default Vue.extend({
             this.showBigImage = true;
         },
         async emptyZip() {
-            const zipTxt = await exportZipFolder(this.dataset.taxonsHierarchy!);
+            const zipTxt = await exportZipFolder(this.selectedTaxon ?? this.dataset.taxonsHierarchy!);
             download(zipTxt, "zip", undefined, true);
         },
         texExport() {
--- src/features/zipfolderexport.ts
+++ src/features/zipfolderexport.ts
@@ -3,19 +3,42 @@ import { Hierarchy, Taxon } from "@/datatypes";
 import generateFileName from "./generatefilename";
 
 
-function getEntries(hierarchy: Hierarchy<Taxon>, entries: Iterable<Taxon>, zip: JSZip, path = "") {
+async function fillZipEntry(hierarchy: Hierarchy<Taxon>, entries: Iterable<Taxon>, zip: JSZip, path = "") {
 	for (const entry of entries) {
 		const entryName = generateFileName(entry.name.S);
 		const currentPath = path + encodeURI(entryName) + "/";
 		zip.folder(currentPath);
-		getEntries(hierarchy, entry.children, zip, currentPath);
+		const futurePictures: Array<Promise<Response>> = [];
+		const paths: string[] = [];
+		for (const [i, pic] of entry.pictures.entries()) {
+			if (pic.hubUrl) {
+				const url = pic.hubUrl;
+				const parts = url.split("/");
+				let fileName = parts[parts.length - 1];
+				const fParts = fileName.split(".");
+				let extension = fParts.length > 1 ? fParts[fParts.length - 1] : undefined;
+				if (!extension) {
+					extension = "jpeg";
+				}
+				const path = `${currentPath}${entry.name.S}-${i+1}.${extension}`;
+				paths.push(path);
+				futurePictures.push(fetch(url));
+			}
+		}
+		const pics = await Promise.allSettled(futurePictures.values());
+		const futureBlobs: Promise<Blob>[] = pics.map((p: any) => p.value.blob());
+		const blobs = await Promise.allSettled(futureBlobs);
+		for (let i = 0; i < paths.length; i++) {
+			zip.file(paths[i], blobs[i].value);
+		}
+		fillZipEntry(hierarchy, entry.children, zip, currentPath);
 	}
 }
 
-export function exportZipFolder(hierarchy: Hierarchy<Taxon>): Promise<Blob> {
+export async function exportZipFolder(hierarchy: Hierarchy<Taxon>): Promise<Blob> {
 	const zip = new JSZip();
 
-	getEntries(hierarchy, hierarchy.children, zip);
+	await fillZipEntry(hierarchy, hierarchy.children, zip);
 
 	return zip.generateAsync({type:"blob"});
 }
