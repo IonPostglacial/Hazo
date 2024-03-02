706fcc05a4bd01a8e8410c522c756d89b3702ca0
Nicolas Galipot <nicolas.galipot@gmail.com>
2024-03-02 14:03:50 +0100

    Improve comparison by having one column per language

--- src/components/ComparisonTab.vue
+++ src/components/ComparisonTab.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex-grow-1 scroll">
+    <div class="flex-grow-1 scroll vertical-scroll">
         <VBox>
             <HBox>
                 <DropDownButton label="Characters">
@@ -14,66 +14,42 @@
                         @select-item="addTaxon">
                     </TreeMenu>
                 </DropDownButton>
+                <MultiSelector :choices="langNames" v-model="langIds">
+                </MultiSelector>
                 <Spacer></Spacer>
                 <button type="button" @click="exportComparison">Export</button>
             </HBox>
             <table class="white-background medium-padding thin-border rounded">
                 <thead class="thin-border">
                     <tr>
-                        <th>Name</th>
-                        <template v-for="char in selectedCharacters">
-                            <th v-if="char.characterType === 'range'">
-                                <HBox class="center-items gap-1">
-                                    <button @click="removeCharacter(char)">
-                                        <font-awesome-icon icon="fa-solid fa-close" />
-                                    </button>
-                                    <div>{{ char.name.S }} Min</div>
-                                </HBox>
-                            </th>
-                            <th v-if="char.characterType === 'range'">
-                                <div>{{ char.name.S }} Max</div>
-                            </th>
-                            <th v-if="char.characterType === 'discrete'">
-                                <HBox class="center-items gap-1">
-                                    <button @click="removeCharacter(char)">
-                                        <font-awesome-icon icon="fa-solid fa-close" />
-                                    </button>
-                                    <div>{{ char.name.S }}</div>
-                                </HBox>
-                            </th>
-                        </template>
+                        <th v-for="col in table.header">
+                            <HBox class="center-items gap-1">
+                                <button v-if="col.type === 'measure' || col.type === 'state'" 
+                                        @click="removeCharacter(col.character)">
+                                    <font-awesome-icon icon="fa-solid fa-close" />
+                                </button>
+                                <div>{{ col.label }}</div>
+                            </HBox>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="taxon in selectedTaxa">
-                        <td>
-                            <HBox class="center-items gap-1">
-                                <button  @click="removeTaxon(taxon)">
+                    <tr v-for="row of table.data">
+                        <td v-for="cell of row.cells">
+                            <HBox v-if="cell.type === 'name'" class="center-items gap-1">
+                                <button  @click="removeTaxon(row.taxon)">
                                     <font-awesome-icon icon="fa-solid fa-close" />
                                 </button>
-                                <div>{{ taxon.name.S }}</div>
+                                <div>{{ cell.value }}</div>
                             </HBox>
-                        </td>
-                        <template v-for="char in selectedCharacters">
-                            <td v-if="char.characterType === 'discrete'">
-                            {{ 
-                                taxon.states
-                                    .filter(state => char.states?.some(s => s.id === state.id))
-                                    .map(s => s.name.S)
-                                    .join(", ")
-                            }}
-                            </td>
-                            <template v-if="char.characterType === 'range'">
-                                <td class="right-text">
-                                    {{ taxon.measurements[char.id]?.min ?? "NA" }}
-                                    {{ char.unit?.name.S }}
-                                </td>
-                                <td class="right-text">
-                                    {{ taxon.measurements[char.id]?.max ?? "NA" }}
-                                    {{ char.unit?.name.S }}
-                                </td>
+                            <template v-if="cell.type === 'measure'">
+                                {{ cell.value ?? "NA" }}
+                                {{ cell.unit?.name.S }}
                             </template>
-                        </template>
+                            <template v-if="cell.type === 'state'">
+                                {{ cell.states.map(s => s.label).join(", ") }}
+                            </template>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -85,20 +61,70 @@
     import { Language } from "@/db-index";
     import DropDownButton from "./toolkit/DropDownButton.vue";
     import TreeMenu from "./toolkit/TreeMenu.vue";
+    import MultiSelector from "./toolkit/MultiSelector.vue";
     import HBox from "./toolkit/HBox.vue";
     import VBox from "./toolkit/VBox.vue";
     import { useDatasetStore } from "@/stores/dataset";
-    import { ref } from "vue";
-    import { Character, Taxon } from "@/datatypes";
+    import { ComputedRef, Ref, computed, ref } from "vue";
+    import { Character, DiscreteCharacter, RangeCharacter, Taxon, Unit } from "@/datatypes";
     import Spacer from "./toolkit/Spacer.vue";
     import download from "@/tools/download";
     import { escape } from "@/tools/parse-csv";
+
+    type AnyColumn = { label: string };
+    type StateColumn = AnyColumn & { type: "state", character: DiscreteCharacter, lang: Language };
+    type MeasureColumn = AnyColumn & { type: "measure", character: RangeCharacter, value: "min" | "max" };
+    type NameColumn = AnyColumn & { type: "name" };
+    type Column = StateColumn | MeasureColumn | NameColumn;
+    type StateCell = { type: "state", column: StateColumn, states: { label: string }[] };
+    type MeasureCell = { type: "measure", column: MeasureColumn, value: number|undefined, unit: Unit|undefined };
+    type NameCell = { type: "name", column: NameColumn, value: string };
+    type DataCell = StateCell | MeasureCell | NameCell;
+    type Row = { taxon: Taxon, cells : DataCell[] };
+    type Table = { header: Column[], data: Row[] };
 
     const taxonNameFields: { label: string, propertyName: Language }[] = [{ label: 'NS', propertyName: 'S' }, { label: 'NV', propertyName: 'V'}, { label: '中文名', propertyName: 'CN' }];
     const characterNameFields: { label: string, propertyName: Language }[] = [{ label: 'FR', propertyName: 'S'}, { label: 'EN', propertyName: 'EN' }, { label: '中文名', propertyName: 'CN' }];
     const ds = useDatasetStore();
     const selectedCharacters = ref<Character[]>([]);
     const selectedTaxa = ref<Taxon[]>([]);
+    const langNames: Ref<Language[]> = ref(["FR", "CN", "EN"]);
+    const langIds = ref([0]);
+    const selectedLangs = computed(() => {
+        return langIds.value.map(id => langNames.value[id]);
+    });
+    const table: ComputedRef<Table> = computed(() => {
+        const nameColumn: NameColumn = { type: "name", label: "Name" };
+        const table: Table = { header: [nameColumn], data: [] };
+        for (const char of selectedCharacters.value) {
+            if (char.characterType === "discrete") {
+                for (const lang of selectedLangs.value) {
+                    table.header.push({ type: "state", label: char.name[lang] ?? "", character: char, lang });
+                }
+            }
+            if (char.characterType === "range") {
+                table.header.push({ type: "measure", label: `${char.name.S} Min`, character: char, value: "min" });
+                table.header.push({ type: "measure", label: "Max", character: char, value: "max" });
+            }
+        }
+        for (const taxon of selectedTaxa.value) {
+            const cells: DataCell[] = [{ type: "name", column: nameColumn, value: taxon.name.S }];
+            for (const column of table.header) {
+                if (column.type === "state") {
+                    cells.push({ type: "state", column, states: taxon.states
+                                .filter(state => column.character.states?.some(s => s.id === state.id))
+                                .map(s => ({ label: s.name[column.lang] ?? "" })) });
+                }
+                if (column.type === "measure") {
+                    const measurement = taxon.measurements[column.character.id];
+                    const value = measurement ? measurement[column.value] : undefined;
+                    cells.push({ type: "measure", column, value, unit: column.character.unit })
+                }
+            }
+            table.data.push({ taxon, cells });
+        }
+        return table;
+    });
 
     function addCharacter(id: string) {
         const selectedChar = ds.characterWithId(id);
@@ -124,21 +150,17 @@
 
     function exportComparison() {
         const csv: string[][] = [];
-        const header = ["Name"];
-        for (const char of selectedCharacters.value) {
-            header.push(escape(char.name.S));
-        }
-        csv.push(header);
-        for (const taxon of selectedTaxa.value) {
-            const line = [taxon.name.S];
-            for (const char of selectedCharacters.value) {
-                line.push(escape(taxon.states
-                            .filter(state => char.states?.some(s => s.id === state.id))
-                            .map(s => s.name.S)
-                            .join(", ")));
+        csv.push(table.value.header.map(h => h.label));
+        csv.push(...table.value.data.map(row => row.cells.map(c => {
+            switch (c.type) {
+                case "name":
+                    return c.value;
+                case "measure":
+                    return `${c.value ?? ''} ${c.unit?.name.S ?? ''}`;
+                case "state":
+                    return c.states.map(s => s.label).join(", ");
             }
-            csv.push(line);
-        }
-        download(csv.map(line => line.join(",")).join("\n"), "csv");
+        })));
+        download(csv.map(line => line.map(cell => escape(cell)).join(",")).join("\n"), "csv");
     }
 </script>
